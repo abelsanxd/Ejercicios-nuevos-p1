af62c5d8fbb997e056e94d829081a2914cc44add
abelsanxd <abelsan4665@gmail.com>
2024-02-25 23:37:45 -0300

    se creo un objeto llamado auto con sus caracteristicas, que permite apagar y encender

---
+++ js/tpej1.js
@@ -0,0 +1,27 @@
+const auto = {
+  color: "rojo",
+  marca: "Toyota",
+  modelo: "Corolla",
+  encendido: false,
+
+  encender: function () {
+    if (!this.encendido) {
+      this.encendido = true;
+      document.write("Auto encendido<br>");
+    } else {
+      document.write("El auto ya está encendido<br>");
+    }
+  },
+
+  apagar: function () {
+    if (this.encendido) {
+      this.encendido = false;
+      document.write("El auto se apagó<br>");
+    } else {
+      document.write("El auto ya está apagado<br>");
+    }
+  },
+};
+
+auto.encender();
+auto.apagar();
